9995f0bf8094b3deba5e6151bdb3d99db629255f
Raúl Garcia <ego.war@icloud.com>
2026-06-02 23:21:53 -0700

    🎨 Font provider trial
    adobe fonts

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -1,11 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
+import { fontProviders } from 'astro/config'
 
 import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://perreo.space',
+
+  fonts: [
+    {
+      provider: fontProviders.fontsource(),
+      name: 'Jacquard 24',
+      cssVariable: '--font-jacquard-24',
+    },
+  ],
 
   vite: {
     plugins: [tailwindcss()],
